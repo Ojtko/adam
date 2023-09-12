16909ec16679bb97cdfbd53c56288c416adafc2d
Ojtko <wojtulo998@gmail.com>
2023-09-12 13:21:31 +0200

    creating to objets Reszta

--- project1/src/com/company/LiczbaHolder.java
+++ project1/src/com/company/LiczbaHolder.java
@@ -1,2 +1,17 @@
-package com.company;public class LiczbaHolder {
+package com.company;
+
+public class LiczbaHolder {
+    int a;
+
+    public LiczbaHolder(int a) {
+        this.a = a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
 }
--- project1/src/com/company/Main.java
+++ project1/src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("cos");
+        LiczbaHolder lh1 = new LiczbaHolder(2);
+        LiczbaHolder lh2 = new LiczbaHolder(30);
+        for(int i = 0; i<lh2.a; i++){
+            lh1.a = lh1.a * lh1.a;
+            System.out.println(lh1.a);
+        }
+
     }
 }
